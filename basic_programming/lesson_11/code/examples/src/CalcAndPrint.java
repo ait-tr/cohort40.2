75c9d5c91194a17ee4bb342ac05e5ff4fc6ee350
SunnyCapt <sunny.capt@tuta.io>
2024-01-30 13:16:46 +0100

    fixed: lesson_11 code

--- basic_programming/lesson_11/code/examples/src/CalcAndPrint.java
+++ basic_programming/lesson_11/code/examples/src/CalcAndPrint.java
@@ -1,15 +1,15 @@
 public class CalcAndPrint {
     public static void main(String[] args) {
         int a = 1;
-        int resultA = calcAndPrint(a);
+        int resultA = calc(a);
         System.out.println(a + " ^ 3 = " + resultA);
 
         int b = 2;
-        int resultB = calcAndPrint(b);
+        int resultB = calc(b);
         System.out.println(b + " ^ 3 = " + resultB);
     }
 
-    public static int calcAndPrint(int myBestNumber) {
+    public static int calc(int myBestNumber) {
         int result = myBestNumber * myBestNumber * myBestNumber;
         return result;
     }
