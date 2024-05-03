98e5911e0863801c3520478d7d42a4a5eafefa42
v1148656 <v1148656@gmail.com>
2024-05-03 18:28:27 +0300

    Add lesson_25

---
+++ front_end/lesson_25/code/24_WeatherApp_02_05_2024/script.js
@@ -0,0 +1,23 @@
+const API_KEY = "fa1e72ff893c6a4a5ed4077327e855b4";
+const locationInput = document.getElementById("locationInput");
+const getWeatherButton = document.getElementById("getWeatherButton");
+const weatherContainer = document.getElementById("weatherContainer");
+
+getWeatherButton.onclick = () => {
+  const cityName = locationInput.value.trim();
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`)
+    .then((response) => response.json())
+    .then((weather) => displayWeather(weather));
+
+  locationInput.value = "";
+};
+
+function displayWeather(weather) {
+    weatherContainer.innerHTML = `
+        <h2>${weather.name}</h2>
+        <p>Temperature: ${weather.main.temp}°C</p>
+        <p>Description: ${weather.weather[0].description}</p>
+        <p>Speed of wind: ${weather.wind.speed} m/s</p>
+    `;
+}
---
+++ front_end/lesson_25/plan.md
@@ -0,0 +1,3 @@
+## Задачи на урок:
+
+1. Module graduation project (Weather App)
